de59428862c5d79f86b80cdf033ab82efa04987f
Tyler-McFarland <mcfarlandtyler@live.com>
2020-04-27 18:38:27 -0500

    Accounts actually going into Database

--- ResearchIT/ResearchITMVC/App_Start/RouteConfig.cs
+++ ResearchIT/ResearchITMVC/App_Start/RouteConfig.cs
@@ -18,6 +18,11 @@ namespace ResearchITMVC
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Account",
+                url: "{controller}/{action}/{name}/{email}/{pass1}"
+            );
         }
     }
 }
---
+++ ResearchIT/ResearchITMVC/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResearchITMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Account()
+        {
+            return View();
+        }
+
+        public ActionResult sendAccount(string name, string email, string pass1)
+        {
+            string connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = ResearchIT; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            string query = $"INSERT INTO Account VALUES ('{email}', '{name}', '{pass1}')";
+
+            SqlCommand command = new SqlCommand(query, conn);
+            command.Connection.Open();
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throwDuplicateAccount(email);
+                throw;
+            }
+            return View("~/Views/AccountCreated.cshtml");
+
+        }
+
+        //private JsonResult throwDuplicateAccount(string email)
+        //{ 
+        //}
+    }
+}
